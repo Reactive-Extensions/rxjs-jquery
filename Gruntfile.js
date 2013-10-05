7c68c30f75d5648ae7f323925517d57a6bc7f0bf
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2013-10-04 22:40:09 -0400

    Added nuget to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         meta: {
             banner:
                 '/*'+
@@ -52,6 +53,38 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('nuget', 'Register NuGet-RxJSjQuery', function () {
+        var done = this.async();
+
+        //invoke nuget.exe
+        grunt.util.spawn({
+            cmd: ".nuget/nuget.exe",
+            args: [
+                //specify the .nuspec file
+                "pack",
+                "nuget/RxJS-Bridges-jQuery/RxJS-Bridges-jQuery.nuspec",
+
+                //specify where we want the package to be created
+                "-OutputDirectory",
+                "nuget",
+     
+                //override the version with whatever is currently defined in package.json
+                "-Version",
+                grunt.config.get("pkg").version
+            ]
+        }, function (error, result) {
+            if (error) {
+                grunt.log.error(error);
+            } else {
+                grunt.log.write(result);
+            }
+
+            done();
+        });     
+    });    
 
     // Default task(s).
     grunt.registerTask('default', ['concat:basic', 'uglify:basic', 'qunit']);
