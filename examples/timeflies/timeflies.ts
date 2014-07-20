a838162241c28b5162930c21fc44af626ebba79a
Devon Stewart <blast@hardchee.se>
2014-07-19 20:23:39 -0700

    Adding timeflies.ts
    Also switching to noconflict-style document.ready

--- examples/timeflies/timeflies.ts
+++ examples/timeflies/timeflies.ts
@@ -1,20 +1,23 @@
 /// <reference path="../../ts/rx.jquery.d.ts" />
-jQuery(function ($) {
+
+jQuery(function ($: JQueryStatic) {
+
     $(function () {
         var text = 'time flies like an arrow',
             container = $('#textContainer'),
             doc = $(document),
             mouseMove = doc.bindAsObservable('mousemove'),
-            mouseMoveOffset = mouseMove.select(function(value) {
+
+            mouseMoveOffset = mouseMove.select(function(value: JQueryMouseEventObject) {
                 var offset = container.offset();
                 return {
-                    offsetX: value.clientX - offset.left,
-                    offsetY: value.clientY - offset.top
-                };
+                   offsetX : value.clientX - offset.left,
+                   offsetY : value.clientY - offset.top
+               };
             });
 
         for (var i = 0, len = text.length; i < len; i++) {
-            (function(i) {
+            (function(i: number) {
                 var s = $('<span/>', {
                     text: text[i],
                     css: {
@@ -22,7 +25,7 @@ jQuery(function ($) {
                     }
                 }).appendTo(container);
 
-                mouseMoveOffset.delay(i * 100).subscribe(function(mouseEvent) {
+                mouseMoveOffset.delay(i * 100).subscribe(function(mouseEvent: JQueryMouseEventObject) {
                     s.css({
                         top: mouseEvent.offsetY + 'px',
                         left: mouseEvent.offsetX + i * 10 + 15 + 'px'
